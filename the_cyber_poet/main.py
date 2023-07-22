a41a6ed55f148af8c49f20d7fb0acc7cfe10f9c7
Christian G <xiangranados@icloud.com>
2023-07-22 10:22:51 +0200

    Initial commit with project structure and basic ChatGPT integration

--- the_cyber_poet/main.py
+++ the_cyber_poet/main.py
@@ -1,3 +1,14 @@
+from .api import chatgpt
 
-if __name__ == '__main__':
-    print("Hello, World!")
+def main():
+    # Get the OpenAI key from environment variables
+    api_key = os.getenv('OPENAI_KEY')
+
+    # Substitute 'your-api-key' with your actual OpenAI API key
+    chatgpt.setup_gpt(api_key)
+
+    prompt = "Once upon a time"
+    print(chatgpt.send_prompt(prompt))
+
+if __name__ == "__main__":
+    main()
